88dcba58072591aa1349ee1372c4f8618b98a72f
Lafayette Pinheiro <lafayette@Lafayettes-MacBook-Pro.local>
2014-05-27 14:31:35 -0300

    apagando

---
+++ Portugues Facil/Portugues Facil/ptf.h
@@ -0,0 +1,13 @@
+//
+//  ptf.h
+//  Portugues Facil
+//
+//  Created by Lafayette Pinheiro on 5/27/14.
+//  Copyright (c) 2014 Lafayette Pinheiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ptf : NSObject
+
+@end
---
+++ Portugues Facil/Portugues Facil/ptf.m
@@ -0,0 +1,13 @@
+//
+//  ptf.m
+//  Portugues Facil
+//
+//  Created by Lafayette Pinheiro on 5/27/14.
+//  Copyright (c) 2014 Lafayette Pinheiro. All rights reserved.
+//
+
+#import "ptf.h"
+
+@implementation ptf
+
+@end
