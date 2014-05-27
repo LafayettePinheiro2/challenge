1e35abafaf54a8f02e38eb0c2b6813adad6e893b
Yoshiharu Takuno <yoshiharutakuno@MacBook-Pro-de-Yoshiharu.local>
2014-05-27 14:25:24 -0300

    commit classe d

---
+++ Portugues Facil/Portugues Facil/ClasseD.h
@@ -0,0 +1,13 @@
+//
+//  ClasseD.h
+//  Portugues Facil
+//
+//  Created by Yoshiharu Takuno on 27/05/14.
+//  Copyright (c) 2014 Lafayette Pinheiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClasseD : NSObject
+
+@end
---
+++ Portugues Facil/Portugues Facil/ClasseD.m
@@ -0,0 +1,13 @@
+//
+//  ClasseD.m
+//  Portugues Facil
+//
+//  Created by Yoshiharu Takuno on 27/05/14.
+//  Copyright (c) 2014 Lafayette Pinheiro. All rights reserved.
+//
+
+#import "ClasseD.h"
+
+@implementation ClasseD
+
+@end
