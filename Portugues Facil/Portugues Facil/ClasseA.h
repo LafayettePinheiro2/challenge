3838c224dc95b48604b015d191f90e5cc36d3347
Lafayette Pinheiro <lafayette@Lafayettes-MacBook-Pro.local>
2014-05-27 14:21:15 -0300

    classe a

---
+++ Portugues Facil/Portugues Facil/ClasseA.h
@@ -0,0 +1,13 @@
+//
+//  ClasseA.h
+//  Portugues Facil
+//
+//  Created by Lafayette Pinheiro on 5/27/14.
+//  Copyright (c) 2014 Lafayette Pinheiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClasseA : NSObject
+
+@end
---
+++ Portugues Facil/Portugues Facil/ClasseA.m
@@ -0,0 +1,13 @@
+//
+//  ClasseA.m
+//  Portugues Facil
+//
+//  Created by Lafayette Pinheiro on 5/27/14.
+//  Copyright (c) 2014 Lafayette Pinheiro. All rights reserved.
+//
+
+#import "ClasseA.h"
+
+@implementation ClasseA
+
+@end
