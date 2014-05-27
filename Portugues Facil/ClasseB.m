2d1cbbf398ed388b57f2e775749ffcbf4b6e4de5
Yoshiharu Takuno <yoshiharutakuno@MacBook-Pro-de-Yoshiharu.local>
2014-05-27 14:20:29 -0300

    Criando classea

---
+++ Portugues Facil/ClasseB.m
@@ -0,0 +1,13 @@
+//
+//  ClasseB.m
+//  Portugues Facil
+//
+//  Created by Yoshiharu Takuno on 27/05/14.
+//  Copyright (c) 2014 Lafayette Pinheiro. All rights reserved.
+//
+
+#import "ClasseB.h"
+
+@implementation ClasseB
+
+@end
